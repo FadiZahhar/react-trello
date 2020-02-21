9ccf740c6e28bde89face3d40b47c036c0ef5b17
Fadi Nicolas Zahhar <fnzahhar@gmail.com>
2020-02-21 22:27:58 +0200

    adding the pages clean

---
+++ src/components/BoardPreview.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+class BoardPreview extends React.Component {
+	render() {
+		return (
+			<p>board preview</p>
+		)
+	}
+}
+
+
+export default BoardPreview;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,30 +1,15 @@
 import React from 'react'
 import BoardPreview from '../BoardPreview'
-import CreateBoardForm from '../CreateBoardForm'
-import PropTypes from 'prop-types';
 
 class Home extends React.Component {
-	componentDidMount() {
-		this.props.getBoards(this.props.match.params.userId)
-	}
+
 	render() {
 		return (
-			< div >
-				<CreateBoardForm createNewBoard={this.props.createNewBoard} />
-				<div className="board-preview-wrapper">
-					{Object.keys(this.props.boards).map(key => (
-						<BoardPreview key={key} board={this.props.boards[key]} />
-					))}
-				</div>
-			</div>
+			<h1>Home</h1>
 		)
 	}
 }
 
-Home.propTypes = {
-	getBoards: PropTypes.func.isRequired,
-	boards: PropTypes.array.isRequired,
-	createNewBoard: PropTypes.func.isRequired
-}
+
 
 export default Home
